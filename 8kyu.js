2594448dd989d17c9a0c11311d825d809249d8ed
Artur <69423332+balsis@users.noreply.github.com>
2022-06-07 21:39:50 +0300

    Update 8kyu.js

--- 8kyu.js
+++ 8kyu.js
@@ -86,3 +86,29 @@ function makeNegative(num) {
     return num=num
   }
 }
+
+// 7. Sum of positive
+
+/*You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.*/
+
+function positiveSum(arr) {
+    let sum=0;
+    let i=0;
+    for (i of arr) { 
+    if (i>0) sum=sum+i;   
+    } 
+    return sum  
+}
+
+function positiveSum(arr) {
+  let sum=0;
+  for (let i=0; i<=arr.length; i++) {   
+  if (arr[i]>0) sum=sum+arr[i];
+  }  
+  return sum
+}
+
