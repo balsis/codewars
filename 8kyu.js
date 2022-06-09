8df5120709921c7b0b7094e44774a82e816ca2de
Artur <69423332+balsis@users.noreply.github.com>
2022-06-09 19:43:13 +0300

    add 10

--- 8kyu.js
+++ 8kyu.js
@@ -159,3 +159,11 @@ const rps = (p1, p2) => {
   }  
   return res
 };
+
+// 10.Remove First and Last Character
+/*
+It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+*/
+function removeChar(str){
+ return res=str.slice(1, -1)
+};
