f4dd85fdd2e50fd51a3c2e12002f363d41d78801
Artur <69423332+balsis@users.noreply.github.com>
2022-07-31 16:56:33 +0300

    Update 8kyu.js

--- 8kyu.js
+++ 8kyu.js
@@ -219,3 +219,13 @@ let x2 = x.toLowerCase();
     }
   else {return false}
 }
+
+// 14. Reversed Strings
+/*Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'*/
+
+function solution(str){
+  return str.split('').reverse().join('');  
+}
